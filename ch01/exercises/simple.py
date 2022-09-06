7a38bfa3d94d30c53a4d98c401e6e59bf1744a98
Kayla Elliott <112587205+Kayla-Elliott@users.noreply.github.com>
2022-09-06 22:05:56 +0000

    added simple.py and worked on classwork inside

---
+++ ch01/exercises/simple.py
@@ -0,0 +1,16 @@
+print(10*5)
+print(10**2) #exponent 
+print(15 / 10)
+print(15 // 10)
+print(-15 // 10) #its rounding down here
+print(15%10)
+print(10%15)
+print(10%10)
+print(0%10)
+print(10/15) #this repeats the decimal
+
+
+print("What is the current rate from Euro to Dollar?")
+rate = input()
+
+#ask the person to input the amount that the rate is, and asign that to the rate variable, ask the user to input an amount ad save that to Amount
