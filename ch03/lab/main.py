871e717ed65157a8e667a1ec9790514dcfc439a4
Kayla Elliott <112587205+Kayla-Elliott@users.noreply.github.com>
2022-09-23 18:31:47 +0000

    Did the ch03 lab

--- ch03/lab/main.py
+++ ch03/lab/main.py
@@ -1,7 +1,12 @@
 import turtle #1. import modules
 import random
+import time 
+import pygame
+import math
 
 #Part A
+
+
 window = turtle.Screen() # 2.  Create a screen
 window.bgcolor('lightblue')
 
@@ -16,11 +21,49 @@ michelangelo.up() # 4. Pick up the pen so we don’t get lines
 leonardo.up()
 michelangelo.goto(-100,20)
 leonardo.goto(-100,-20)
-
+time.sleep(3)
 ## 5. Your PART A code goes here
 
+michelangelo.forward(random.randrange(1,101))
+time.sleep(2)
+leonardo.forward(random.randrange(1,101))
+time.sleep(2)
+michelangelo.goto(-100,20)
+leonardo.goto(-100,-20)
+time.sleep(2)
 
-# PART B - complete part B here
+for _ in range(10):
+  michelangelo.forward(random.randrange(1,11))
+  time.sleep(2)
+  leonardo.forward(random.randrange(1,11))
+  time.sleep(2)
 
+michelangelo.goto(-100,20)
+leonardo.goto(-100,-20)
 
 window.exitonclick()
+# PART B - complete part B here
+pygame.init()
+window = pygame.display.set_mode()
+
+coords = []
+numb_sides  =  [3,4,6,9,360]
+side_length =  50
+offset = 100
+window.fill(pygame.Color("gray96"))
+
+for i in range(5):
+  for _ in range(numb_sides[i]):  
+   angle = (2.0 * math.pi * (_)) / numb_sides[i]
+   x = side_length * math.cos(angle) + offset
+   y = side_length * math.sin(angle) + offset
+   coords.append([x,y])
+  pygame.draw.polygon(window, "hotpink", coords) 
+  pygame.display.flip()
+  coords.clear()
+  pygame.time.wait(1000)
+  window.fill(pygame.Color("gray96"))
+
+
+
+#window.exitonclick()
